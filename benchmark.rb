99b1d5800cff0b736a1d5815765fc61936d31253
Freddie Manners <f.manners@gmail.com>
2010-04-04 00:08:33 +0100

    Refactored a lot and added database backend support, which does something at any rate.

--- benchmark.rb
+++ benchmark.rb
@@ -1,5 +1,8 @@
 require 'net/http'
 require 'cgi'
+require 'mysql'
+
+require 'config'
 
 srand(0)
 
@@ -20,58 +23,85 @@ def random_byte_string(len)
    return str
 end
 
-def generate_random_file()
-   return { :info_hash => random_byte_string(20) }
+class Benchmarker
+   private
+
+   def generate_random_client()
+      hash = {}
+
+      hash[:peer_id] = random_byte_string(20)
+      hash[:port] = rand(10000) + 30000
+      hash[:compact] = rand(2)
+      hash[:numwant] = rand(4000)
+      hash[:no_peer_id] = rand(2)
+
+      return hash
+   end
+
+   def generate_random_status()
+      hash = {}
+
+      hash[:uploaded] = rand(100000000)
+      hash[:downloaded] = rand(100000000)
+      hash[:event] = ["started", "stopped", "completed"][rand(3)]
+      hash[:left] = rand(100000000)
+
+      return hash
+   end
+
+   public
+
+   def initialize()
+      @db = Mysql.real_connect('localhost', MYSQL_USER, MYSQL_PASS, MYSQL_DB)
+      @db.reconnect = true
+
+      print "Initializing random data ..."
+      $stdout.flush()
+
+      hashes = []
+      @db.query("SELECT info_hash FROM torrents").each_hash do |h|
+         hashes.push(h)
+      end
+      @files = hashes
+
+      users = []
+      @db.query("SELECT ID FROM users_main").each_hash do |h|
+         users.push(nil)
+      end
+      @clients = users.map { generate_random_client() }
+
+      @no_files = @files.size()
+      @no_clients = @clients.size()
+
+      puts " done!\n"
+   end
+
+   def make_request()
+      file = @files[rand(@no_files)]
+      client = @clients[rand(@no_clients)]
+      status = generate_random_status()
+
+      str = "/announce?" + file.merge(client).merge(status).to_url_params() + " HTTP/1.1"
+
+      t0 = Time.now()
+      response = Net::HTTP::get("0.0.0.0", str, "6969")
+      t1 = Time.now()
+
+      print "Called:\n\t"
+      puts str
+      print "Received:\n\t"
+      p response
+      puts "in #{t1 - t0} seconds\n"
+
+      return nil
+   end
+
+   def go()
+      while true
+         make_request()
+      end
+   end
 end
 
-def generate_random_client()
-   hash = {}
-
-   hash[:peer_id] = random_byte_string(20)
-   hash[:port] = rand(10000) + 30000
-   hash[:compact] = rand(2)
-   hash[:numwant] = rand(4000)
-   hash[:no_peer_id] = rand(2)
-
-   return hash
-end
-
-def generate_random_status()
-   hash = {}
-
-   hash[:uploaded] = rand(100000000)
-   hash[:downloaded] = rand(100000000)
-   hash[:event] = ["started", "stopped", "completed"][rand(3)]
-   hash[:left] = rand(100000000)
-
-   return hash
-end
-
-$no_files = 100000
-$no_clients = 100000
-
-puts "Initializing random data ..."
-
-$files = Array.new($no_files) { generate_random_file() }
-$clients = Array.new($no_clients) { generate_random_client() }
-
-puts " done\n"
-
-def make_request()
-   file = $files[rand($no_files)]
-   client = $clients[rand($no_clients)]
-   status = generate_random_status()
-
-   str = "/announce?" + file.merge(client).merge(status).to_url_params() + " HTTP/1.1"
-   response = Net::HTTP::get("0.0.0.0", str, "6969")
-
-   puts str
-   p response
-   puts ""
-
-   return nil
-end
-
-while true
-   make_request()
-end
+benchmarker = Benchmarker.new()
+benchmarker.go()
